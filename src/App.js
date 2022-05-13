f8ddea71b57d21e6497393a8d0924d77becb89fd
Ajjay05 <official.adamya@gmail.com>
2022-05-13 11:30:17 +0530

    firebase1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [field, setField] = useState({
+
+    email: "",
+    password: ""
+
+  })
+
+
+
+  let name, val;
+
+  const store = (e) => {
+    val = e.target.value;
+    name = e.target.name;
+    setField({ ...field, [name]: val })
+
+  }
+
+  const save = async (e) => {
+    e.preventDefault();
+
+    const { email, password } = field;
+
+    const res = await fetch("https://fir-connect-d5242-default-rtdb.firebaseio.com/logindatabase.json", {
+
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json" 
+      },
+      body: JSON.stringify({
+        email, password
+
+      })
+    })
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='bigdiv'>
+        <div className='container'>
+        <form method='POST' onSubmit={save} >
+          
+          <div><h1 className='flex'>Login</h1></div>
+          <div className='button'><input className='flex username' type="text" name="email" value={field.email} onChange={store} placeholder='Enter Email' required></input></div>
+          <div className='button'><input className='flex password' type="password" name="password" value={field.password} placeholder='Password' onChange={store} required></input></div>
+          <div className='button'><button className=' login' type="submit" >Sign in</button></div>
+        </form>
+        </div>
+      </div>
+
+
+    </>
+  )
 }
 
 export default App;
